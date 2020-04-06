005a6b95f949f712e9d9e5d95d199442ccf45d89
DrNuke <chris@boles.ch>
2020-04-06 02:48:13 +0200

    inimum working example. only dividends/ WHT so far

--- src/drnukebean/importer/ibkr.py
+++ src/drnukebean/importer/ibkr.py
@@ -12,6 +12,8 @@ import pandas as pd
 from datetime import datetime, timedelta
 import xml.etree.ElementTree as ET
 import warnings
+import pickle
+import re
 
 import yaml
 from os import path
@@ -45,6 +47,16 @@ def DecimalOrZero(value):
     except:
         return Decimal(0.0)
         
+def AmountAdd(A1,A2):
+    # add two amounts 
+    if A1.currency == A2.currency:
+        quant=A1.number+A2.number
+        return amount.Amount(quant,A1.currency)
+    else:
+        raise('Cannot add amounts of differnent currencies: {} and {}'.format(
+            A1.currency,A1.currency))
+
+
 class IBKRImporter(importer.ImporterProtocol):
     """
     Beancount Importer for the Interactive Brokers XML FlexQueries
@@ -54,39 +66,42 @@ class IBKRImporter(importer.ImporterProtocol):
                 currency='CHF',
                 divSuffix = 'Div', # suffix for dividend Account , like Assets:Invest:IB:VT:Div
                 WHTSuffix = 'WTax', # 
-                fpath=None,
-                deposits=False,
+                fpath=None,  # 
+                deposits=False
                 ):
+
         self.Mainaccount = Mainaccount # main IB account in beancount
         self.currency = currency # main currency of IB account
-        self.divSuffix = divSufix
+        self.divSuffix = divSuffix
         self.WHTSuffix = WHTSuffix
         self.filepath=fpath # optional file path specification, 
-            # if flex query cant be used online
+            # if flex query should not be used online (loading time...)
         self.deposits = deposits # Cash deposits are usually already covered
             # by checkings account statements. If you want anyway the 
             # deposit transactions, provide a True value 
         self.flag = '*' 
 
+    def identify(self, file):
+        return 'ibkr.yaml' == path.basename(file.name)
 
     def getLiquidityAccount(self,currency):
-        return self.Mainaccount + currency  
+        return ':'.join([self.Mainaccount , currency  ])
     
     def getDivIncomeAcconut(self,currency,symbol):
-        return self.Mainaccount.replace('Asset','Income') + symbol + currency
+        return ':'.join([self.Mainaccount.replace('Asset','Income') , symbol , currency])
 
     def getProductAccount(self,symbol):
-        return self.Mainaccount + symbol + symbol
+        return ':'.join([self.Mainaccount , symbol , symbol])
 
     def getWHTAccount(self,symbol,currency):
-        return self.Mainaccount.replace('Asset','Expenses') + symbol + self.WHTSuffix + currency
+        return ':'.join([self.Mainaccount.replace('Asset','Expenses') , symbol , self.WHTSuffix , currency])
 
     def file_account(self, _):
         return self.account
     
     def extract(self, credsfile, existing_entries=None):
         # the actual processing of the flex query
-
+        
         # get the IBKR creentials ready
         try:
             with open(credsfile.name, 'r') as f:
@@ -94,25 +109,31 @@ class IBKRImporter(importer.ImporterProtocol):
                 token = config['token']
                 queryId = config['queryId']
         except:
-            print('cannot read IBKR credentials file. Check filepath.')
+            warnings.warn('cannot read IBKR credentials file. Check filepath.')
             return[]
 
         # get prices of existing transactions, in case we sell something
         priceLookup = PriceLookup(existing_entries, config['baseCcy'])
 
-        # get the report from IB. might take a while, when IB is queuing due to 
-        # traffic
-        try:
-            # try except in case of connection interrupt
-            # Warning: queries sometimes take a few minutes until IB provides
-            # the data due to busy servers
-            response = client.download(token, queryId)
-            statement = parser.parse(response)
-        except:
-            print('could not fetch IBKR Statement')
-            # another option would be to try again
-            return[]
-        assert isinstance(statement, Types.FlexQueryResponse)
+
+        if self.filepath == None:
+            # get the report from IB. might take a while, when IB is queuing due to 
+            # traffic
+            try:
+                # try except in case of connection interrupt
+                # Warning: queries sometimes take a few minutes until IB provides
+                # the data due to busy servers
+                response = client.download(token, queryId)
+                statement = parser.parse(response)
+            except:
+                warnings.warn('could not fetch IBKR Statement. exiting.')
+                # another option would be to try again
+                return[]
+            assert isinstance(statement, Types.FlexQueryResponse)
+        else:
+            print('loading from pickle')
+            with open(self.filepath,'rb') as pf:
+                statement = pickle.load(pf)
        
         # convert to dataframes
         poi=statement.FlexStatements[0] # point of interest
@@ -143,42 +164,45 @@ class IBKRImporter(importer.ImporterProtocol):
         # make dividend & WHT transactions
         for idx, row in match.iterrows():
             
-            currency_div=row['currency']
+            currency=row['currency_x']
+            currency_wht=row['currency_y']
+            if currency != currency_wht:
+                warnings.warn('Warngin: Dividend currency {} ' +
+                    'mismatches WHT currency {}. Skipping this' +  
+                    'Transaction'.format(currency,currency_wht))
+                continue
             symbol=row['symbol']
-            amount_div=row['amount_x']
-            amount_wht=row['amount_y']
+            
+            amount_div=amount.Amount(row['amount_x'],currency)
+            amount_wht=amount.Amount(row['amount_y'],currency)
+
+            text=match['description_x'][2]
+            isin=re.findall('([a-zA-Z]{2}[0-9]{10})',text)[0]
+            pershare=re.search('(\d*[.]\d*)(\D*)(PER SHARE)', 
+                                text, re.IGNORECASE).group(1)
             
             # make the postings, three for dividend/ wht transactions
             postings=[data.Posting(self.getDivIncomeAcconut(currency,
                                                             symbol),
-                                    -amount_div,
-                                    None,
-                                    None,
-                                    None,
-                                    None),
+                                    -amount_div, None, None, None, None),
+                                   
                         data.Posting(self.getWHTAccount(symbol,
                                                         currency),
-                                    amount_wht,
-                                    None,
-                                    None,
-                                    None,
-                                    None),
+                                    -amount_wht, None, None, None, None),
                         data.Posting(self.getLiquidityAccount(currency),
-                                    amount_div + amount_wht,
-                                    None,
-                                    None,
-                                    None,
-                                    None)
+                                    AmountAdd(amount_div,amount_wht),
+                                    None, None, None, None)
                         ]
-
-            data.Transaction(data.new_metadata('dividend', 0), # could add div per share, ISIN,....
+            meta=data.new_metadata('dividend',0,{'ISIN':isin,'per share':pershare})
+            transactions.append(
+                data.Transaction(meta, # could add div per share, ISIN,....
                             row['reportDate'],
                             self.flag,
-                            '',     # payee
-                            row['description'],
+                            symbol,     # payee
+                            'Dividend '+symbol,
                             data.EMPTY_SET,
                             data.EMPTY_SET,
                             postings
-                            )
-                
-        return entries
+                            ))
+        
+        return transactions
